f91c3f25b4bf194d558d4cb2374870325fa18175
Aliaksei.Pitsukha <tonard89@gmail.com>
2022-10-20 18:38:21 +0300

    added new components and sent props between them

---
+++ src/site/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type HeaderPropsType = {
+    titleForHeader: string
+}
+
+export const Header = (props: HeaderPropsType) => {
+    return (
+        <>
+            {props.titleForHeader}
+        </>
+    );
+};
